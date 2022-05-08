29da1c749491c4103aabf888455cda3b21328976
Saulul <poison.mehrdad@gmail.com>
2022-05-09 03:35:51 +0430

    Banners update

--- src/components/Banners.js
+++ src/components/Banners.js
@@ -6,6 +6,9 @@ import '../css/style.css';
 
 //Video
 
+//images
+import banner_1 from '../images/banner-1.jpg';
+import banner_2 from '../images/banner-2.jpg';
 
 
 export default function Banners()
@@ -15,12 +18,13 @@ export default function Banners()
             <section className='bannerSection'>
                 <div className='bannersSectionInnerContainer'>
                     <div className='banners'>
-                        <div className='banner'>Banner</div>
-                        <div className='banner'>Banner</div>
-                        <div className='banner'>Banner</div>
-                        <div className='banner'>Banner</div>
+                        <div className='banner'>
+                            <img alt="banner" src={banner_1}/>
+                        </div>
+                        <div className='banner'>
+                            <img alt="banner" src={banner_2}/>
+                        </div>
                     </div>
-
                     <div className='video'><strong>Video Placeholder</strong></div>
                 </div>
 
