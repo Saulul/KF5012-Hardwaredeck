27f374c4af2ddd8c8ef974f269f5f5675b3292d5
Saulul <poison.mehrdad@gmail.com>
2022-05-10 04:02:21 +0430

    update

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 // This is your test secret API key.
 const stripe = require('stripe')('sk_test_51KwxvYGVoSBOtXM2XmVvIJnpCktSICKIYcdWijKEqLtIalOrFyCszRl2iNPLor2fjrOQmuAyTisoVL1v8s8RJvpO00xKsdBlUc');
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(express.json())
+app.use(
+    cors({
+        origin: "http://localhost:3000/",
+    })
+)
 
 const YOUR_DOMAIN = 'http://localhost:4242';
 
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                price: 'price_1KwyVNGVoSBOtXM2T0zYX0E6',
-                quantity: 1,
-            },
-        ],
-        mode: 'payment',
-        success_url: `http://localhost:3000/?success=true`,
-        cancel_url: `http://localhost:3000/?canceled=true`,
-    });
+app.post("/create-checkout-session", async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: req.body.items.map(item => {
+                return {
+                    price_data: {
+                        currency: "gbp",
+                        product_data: {
+                            name: item.name,
+                        },
+                        unit_amount: (item.price*100).toFixed(0),
+                    },
+                    quantity: item.quantity,
+                }
+            }),
+            success_url: `${YOUR_DOMAIN}/order_success`,
+            cancel_url: `${YOUR_DOMAIN}/checkout`,
+        })
+        res.json({ url: session.url })
+    } catch (e) {
+        res.status(500).json({ error: e.message })
+    }
+})
 
-    res.redirect(303, session.url);
-});
-
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log('Stripe server running on port 4242'));
